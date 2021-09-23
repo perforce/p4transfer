986731cc2d9689cb63cda92d95921c36b158c41f
Robert Cowham <rcowham@perforce.com>
2021-09-23 16:25:33 +0100

    Handle binary files added with ignore integrations

--- P4Transfer.py
+++ P4Transfer.py
@@ -539,6 +539,12 @@ class ChangeRevision:
                 return True
         return False
 
+    def hasOnlyIgnoreIntegrations(self):
+        for integ in self._integrations:
+            if integ.how not in ["ignored"]:
+                return False
+        return True
+
     def integrations(self):
         "Yield in reverse order so that we replay correctly"
         for ind, integ in reversed(list(enumerate(self._integrations))):
@@ -670,7 +676,7 @@ class ChangelistComparer(object):
         diffs = srcfiles.difference(targfiles)
         if diffs:
             # Check for no filesize or digest present - indicating "p4 verify -qu" should be run
-            new_diffs = [r for r in diffs if r.fileSize is None or r.digest is None]
+            new_diffs = [r for r in diffs if r.fileSize and r.digest]
             if not new_diffs:
                 self.logger.debug("Ignoring differences due to lack of fileSize/digest")
                 return (True, "")
@@ -1405,7 +1411,8 @@ class P4Target(P4Base):
     def replicateBranch(self, file, dirty=False):
         # An integration where source has been obliterated will not have integrations
         self.logger.debug('replicateBranch')
-        if not self.options.ignore_integrations and file.hasIntegrations() and file.getIntegration().localFile:
+        if not self.options.ignore_integrations and not file.hasOnlyIgnoreIntegrations() and \
+            file.hasIntegrations() and file.getIntegration().localFile:
             afterAdd = False
             if not self.currentFileContent and os.path.exists(file.fixedLocalFile):
                 self.currentFileContent = readContents(file.fixedLocalFile)
@@ -1464,6 +1471,11 @@ class P4Target(P4Base):
             if diskFileContentModified(file):
                 self.logger.warning('Resyncing add due to file content changes')
                 self.src.p4cmd('sync', '-f', file.localFileRev())
+            if file.hasIntegrations() and file.hasOnlyIgnoreIntegrations():
+                self.logger.debug('processing:0235 ignores')
+                for ind, integ in file.integrations():
+                    self.doIntegrate(file.localIntegSource(ind), file.localFile)
+                    self.p4cmd('resolve', '-ay', file.fixedLocalFile)
 
     def integrateContentsChanged(self, file):
         "Is the source of integrated different to target"
--- test/TestP4Transfer.py
+++ test/TestP4Transfer.py
@@ -1750,6 +1750,40 @@ class TestP4Transfer(unittest.TestCase):
         self.assertNotEqual(filelog[0]['digest'][0], filelog[1]['digest'][0])
         self.assertNotEqual(filelog[0]['fileSize'][0], filelog[1]['fileSize'][0])
 
+    def testBranchIgnoreBinary(self):
+        "Branch with ignore binary file"
+        self.setupTransfer()
+
+        inside = localDirectory(self.source.client_root, "inside")
+        inside_file1 = os.path.join(inside, "inside_file1")
+        inside_file2 = os.path.join(inside, "inside_file2")
+
+        create_file(inside_file1, "Test content")
+        self.source.p4cmd('add', '-tbinary', inside_file1)
+        self.source.p4cmd('submit', '-d', 'inside_file1 added')
+
+        create_file(inside_file2, "Other content")
+        self.source.p4cmd('add', '-tbinary', inside_file2)
+
+        self.source.p4cmd('integ', inside_file1, inside_file2)
+        self.source.p4cmd('resolve', '-ay')
+        self.source.p4cmd('submit', '-d', 'inside_file2 created')
+
+        filelog = self.source.p4cmd('filelog', '//...')
+        self.assertEqual(2, len(filelog))
+        self.assertNotEqual(filelog[0]['digest'][0], filelog[1]['digest'][0])
+        self.assertNotEqual(filelog[0]['fileSize'][0], filelog[1]['fileSize'][0])
+
+        self.run_P4Transfer()
+        self.assertCounters(2, 2)
+
+        filelog = self.target.p4cmd('filelog', '//...')
+        self.assertEqual(2, len(filelog))
+        self.assertNotEqual(filelog[0]['digest'][0], filelog[1]['digest'][0])
+        self.assertNotEqual(filelog[0]['fileSize'][0], filelog[1]['fileSize'][0])
+        filelog = self.target.p4.run_filelog('//depot/import/inside_file2')
+        self.assertEqual(filelog[0].revisions[0].integrations[0].how, "ignored")
+
     def testSimpleIntegrate(self):
         "Simple integration options - inside client workspace view"
         self.setupTransfer()
