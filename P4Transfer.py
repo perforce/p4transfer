75604609578bd25a33f57a88e2d940f92cbce2dd
Robert Cowham <rcowham@perforce.com>
2022-04-18 10:42:49 +0100

    Adjust previous fix

--- P4Transfer.py
+++ P4Transfer.py
@@ -1141,11 +1141,11 @@ class P4Source(P4Base):
                     ))
             chRev.deleteIntegrations(integsToDelete)
 
-    def adjustLocalFileCase(self, changeNum, fileRevs):
+    def adjustLocalFileCase(self, fileRevs):
         """Adjusts case of local files if synced differently"""
         if self.options.case_sensitive or platform.system() != "Linux":
             return
-        haveList = self.srcp4.run('have', '//{}/...@={}'.format(self.P4CLIENT, changeNum))
+        haveList = self.p4.run('have', '//{}/...'.format(self.P4CLIENT))
         localHaveFiles = {}
         for f in haveList:
             k = f['depotFile'].lower()
@@ -1233,7 +1233,7 @@ class P4Source(P4Base):
                 chRev = filesToLog[flog.depotFile]
                 chRev.updateDigest()
         self.abortIfUnsyncableUTF16FilesExist(syncCallback, changeNum)  # May raise exception
-        self.adjustLocalFileCase(changeNum, fileRevs)
+        self.adjustLocalFileCase(fileRevs)
         if self.options.historical_start_change: # Extra processing required on integration records
             self.adjustHistoricalIntegrations(fileRevs)
         if specialMovesSupported():
