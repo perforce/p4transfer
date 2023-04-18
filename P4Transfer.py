e04def60a8a4dc7c23ff6175b9e89bc5d76c8dda
Robert Cowham <rcowham@perforce.com>
2023-04-18 13:58:33 +0100

    p4 -Ztag changes is UTC format time

--- P4Transfer.py
+++ P4Transfer.py
@@ -1752,8 +1752,8 @@ class P4Target(P4Base):
             return
         newChange = self.p4.fetch_change(newChangeId)
         newChange._user = change['user']
-        # date in change is in epoch time, we translate by adding offset
-        newDate = datetime.fromtimestamp(int(change['time']) + targetTimeFromSource.offsetSeconds()).strftime("%Y/%m/%d %H:%M:%S")
+        # date in change is in epoch time (from -Ztag changes - not from change -o), we translate by adding offset
+        newDate = datetime.utcfromtimestamp(int(change['time']) + targetTimeFromSource.offsetSeconds()).strftime("%Y/%m/%d %H:%M:%S")
         newChange._date = newDate
         self.p4.save_change(newChange, '-f')
 
